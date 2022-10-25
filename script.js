929049ea519b2cd2f1a056cf965c7ed2029b84dc
Maria Teresa <37987077+mariaaseret@users.noreply.github.com>
2022-10-25 00:34:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ function DisplayInfo(){
     visit = 0;
     visit ++ ;
     SetCookie("visit", visit, expdate, "/", null, false);
-    document.write("<h5>Esta é a sua "+ visit + "ª visita</h5>"); }
+    document.write("<div class="container"><h5>Esta é a sua "+ visit + "ª visita</h5></div>"); }
 
 document.onload=DisplayInfo();
 
